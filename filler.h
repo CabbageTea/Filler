ec527af75a5893b67f6b87f67878a61175adc8cc
CabbageTea <glaser.des@gmail.com>
2018-01-13 01:00:10 -0800

    seg faults, saves all coords, checks valid places

--- filler.h
+++ filler.h
@@ -13,11 +13,14 @@ typedef struct			s_parse
 	int		plnum; // player number
 	int		width; // width of board
 	int		height; // height of board 
-	int		*firstp[1];
-	int		*pcoords[100]; // piece coordinates
+	int		firstpx;
+	int		firstpy;
+	int		pcoordy[100]; // piece coordinates
+	int		pcoordx[100];
 	char	smine;
 	char	mine; // my piece
 	char	enemy; // enemy piece
+	char	senemy;
 	int		psizex; // piece grid size
 	int		psizey; // ^
 	char	*board[101]; // where the entire board is saved - pls forgive me
@@ -25,11 +28,13 @@ typedef struct			s_parse
 	int		*coordsy; // int array of coords
    	int 	*coordsx; // int array of pcoords
 	int		gameover; // flag that tell us to stop the game
-
+	int 	targety;
+	int		targetx;
 	int		quad_a;
 	int 	quad_b;
 	int		quad_c;
 	int		quad_d;
+	int		stars;
 
 	
 
--- help.c
+++ help.c
@@ -1,5 +1,18 @@
 #include "filler.h"
 
+void	ft_setenemy(t_map *grid)
+{
+	if (grid->mine == 'O')
+	{
+		grid->enemy = 'X';
+		grid->senemy = 'x';
+	}
+	else
+	{
+		grid->enemy = 'O';
+		grid->senemy = 'o';
+	}
+}
 
 void	ft_setplayer(t_map *grid)
 {
@@ -20,6 +33,7 @@ void	ft_setplayer(t_map *grid)
 		grid->mine = 'X';
 		grid->mine = 'x';
 	}
+	ft_setenemy(grid);
 	free(line);
 }
 
@@ -123,6 +137,7 @@ int		ft_countstars(t_map *grid)
 		}
 		x++;
 	}
+	grid->stars = stars;
 	return (stars);
 }
 
@@ -142,8 +157,8 @@ void	ft_therest(int stars, t_map *grid, int x, int y)
 					y++;
 				else
 				{
-					grid->pcoordx[i] = y;
-					grid->pcoordy[i] = x; //actually got this backwards but whatev
+					y = grid->firstpy - grid->pcoordx[i];
+					x = grid->firstpx - grid->pcoordy[i]; //actually got this backwards but whatev
 					i++;
 				}
 			}
@@ -216,30 +231,98 @@ void	ft_readpiece(t_map *grid)
 	ft_savepiece(grid);
 }
 
-void	ft_answer(t_map *grid)
+int		ft_splitquad(int wmin, int wmax, int hmin, int hmax, t_map *grid)
 {
-	if (grid->plnum == 1)
-	{
-		grid->targetx = grid->width;
-		grid->targety = grid->height;
-	}	
-	else
-	{
-		grid->targetx = 0;
-		grid->targety = 0;
-	}
-/*	if (grid->targetx = grid->width)
-		ft_attack1;
-	
-	else
+	int i;
+	int x;
+	int ret;
 
-	ft_attack2;
-	*/
+	ret = 0;
+	x = wmin;
+	i = hmin;
+
+	while (i <= hmax)
+	{
+		x = wmin;
+		while (x <= wmax)
+		{
+			if (grid->board[i][x] == '.')
+				ret++;
+			x++;
+		}
+		i++;
+	}
+	return (ret);
+}
+void	ft_findtarget(t_map *grid, int a, int b, int c, int d)
+{
+	a = grid->quad_a;
+	b = grid->quad_b;
+	c = grid->quad_c;
+	d = grid->quad_d;
+
+	if (d <= a && d <= b && d <= c)
+	{
+		grid->targety = grid->height;
+		grid->targetx = grid->width;
+	}
+	if (c <= d && c <= b && c <= a)
+		grid->targety = grid->height;
+	if (b <= d && b <= c && b <= a)
+		grid->targetx = grid->width;
 }
 
+int		ft_isfree(t_map *grid, int x, int y, int i)
+{
+	int overlap;
+
+	overlap = 0;
+	if (grid->board[y][x] == grid->mine || grid->board[y][x] == grid->smine)
+		overlap++;	
+	while(i < grid->stars)
+	{
+		if (grid->board[y + grid->pcoordy[i]][x + grid->pcoordx[i]] == grid->mine ||
+				grid->board[y + grid->pcoordy[i]][x + grid->pcoordx[i]] == grid->smine)
+			overlap++;
+		if (grid->board[y + grid->pcoordy[i]][x + grid->pcoordx[i]] == grid->enemy
+			   || grid->board[y + grid->pcoordy[i]][x + grid->pcoordx[i]] == grid->senemy
+			|| overlap > 1)
+		   return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_findbestplace(t_map *grid, int x, int y)
+{
+	y = grid->targety;
+	x = grid->targetx;
+	if (grid->board[y][x] == '.' || grid->board[y][x] == grid->smine
+			|| grid->board[y][x] == grid->mine)
+	{
+		if (ft_isfree(grid, x, y, 0))
+			x++;
+	}
+
+}
+void	ft_answer(t_map *grid)
+{
+	int halfw;
+	int halfh;
+
+	halfw = (grid->width / 2);
+	halfh = (grid->height / 2);
+
+	grid->quad_a = ft_splitquad(0, halfw, 0, halfh, grid);
+	grid->quad_b = ft_splitquad(halfw, grid->width - 1, 0, halfh, grid);
+	grid->quad_c = ft_splitquad(0, halfw, halfh, grid->height - 1, grid);
+	grid->quad_d = ft_splitquad(halfw, grid->width - 1, halfh, grid->height - 1, grid);
+	ft_findtarget(grid, 0 , 0 , 0, 0);
+	ft_findbestplace(grid, 0, 0);
+}
 
 int		main(void)
-{	
+{
 	t_map grid;
 	ft_setplayer(&grid);
 	ft_setboardsize(&grid);
